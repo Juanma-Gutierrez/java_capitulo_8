35f5a303a5467960a3a3c0ca4c2ef48b690dd15b
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-11-05 09:46:40 +0100

    fix: Ejercicio18

--- Ejercicio18.java
+++ Ejercicio18.java
@@ -7,19 +7,17 @@
  */
 
 /*
- * Escribe un programa que pase de binario a decimal.
+ * Escribe un programa que pase de decimal a binario.
  */
 
 import java.util.Scanner;
+import matematicas.General;
 
 public class Ejercicio18 {
     public static void main(String[] args) {
         // Var declarations
-        int num;
-        int aux;
-        String[] base = { "0", "1" };
         String res;
-        int resInt;
+        String numero;
 
         // Var init
         res = "";
@@ -27,19 +25,14 @@ public class Ejercicio18 {
         // Scanner class
         Scanner sc = new Scanner(System.in);
 
-        // Process
-        System.out.print("Introduce un número entero: ");
-        num = sc.nextInt();
-        aux = num;
+        // Input data
+        System.out.print("Por favor, introduzca un número decimal: ");
+        numero = sc.next();
 
-        while (aux > 0) {
-            res = base[aux % 2] + res;
-            aux = (int) aux / 2;
-        }
-        resInt = Integer.parseInt(res);
+        res = General.pasaDeDecimal(numero, 2);
 
         // Output results
-        System.out.print("El número " + num + " convertido de decimal a binario queda así: " + resInt);
+        System.out.print("El número " + numero + " convertido de decimal a binario queda así: " + res);
 
         // Close scanner
         sc.close();
