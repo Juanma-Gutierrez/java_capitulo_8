03a2d4cd6962abae1ff215697f9179b368c62071
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-11-11 09:30:04 +0100

    feat: Ejercicio42

---
+++ Ejercicio42.java
@@ -0,0 +1,60 @@
+/**
+ * Exercise: 42
+ *
+ * @version: 11/11/2022
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+/*
+ *
+ * Realiza un programa que pinte un triángulo hueco tal como se muestra en los ejemplos. El usuario debe introducir la altura de la figura. Utiliza funciones para pintar las líneas.
+
+Ejemplo 1:
+Introduzca la altura de la figura: 8
+XXXXXXXX
+X     X
+X    X
+X   X
+X  X
+X X
+XX
+X
+
+Ejemplo 2:
+Introduzca la altura de la figura: 5
+XXXXX
+X  X
+X X
+XX
+X
+ */
+
+import java.util.Scanner;
+import matematicas.General;
+
+public class Ejercicio42 {
+    public static void main(String[] args) {
+        // Var init
+        int height;
+        String charac;
+
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // Input data
+        System.out.print("Introduzca la altura de la figura: ");
+        height = sc.nextInt();
+        System.out.print("Introduzca el carácter que quiere imprimir: ");
+        charac = sc.nextLine();
+        charac = sc.nextLine();
+
+        // Process
+        General.imprimeLinea(height, charac, false);
+        for (int i = height - 1; i > 0; i--)
+            General.imprimeLinea(i, charac, true);
+
+        // Close scanner
+        sc.close();
+    }
+}
